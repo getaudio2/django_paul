cb8bb039ebd2eadfb509b91dd2aaf11aec8a2146
getaudio2 <paugermaisar@hotmail.es>
2025-02-10 18:08:49 +0100

    configuració projecte

--- ACTIVITAT_13/TIC_BCN_PMS/TIC_BCN_PMS/settings.py
+++ ACTIVITAT_13/TIC_BCN_PMS/TIC_BCN_PMS/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'centre',
     'pasApas',
+    'login',
 ]
 
 MIDDLEWARE = [
--- ACTIVITAT_13/TIC_BCN_PMS/TIC_BCN_PMS/urls.py
+++ ACTIVITAT_13/TIC_BCN_PMS/TIC_BCN_PMS/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('centre/', include('centre.urls')),
     path('pasApas/', include('pasApas.urls')),
+    path('login/', include('login.urls')),
 ]
