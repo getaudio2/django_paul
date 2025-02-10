08e282178b62df0631cf20f131d9de645a2f129d
getaudio2 <paugermaisar@hotmail.es>
2025-02-10 18:28:12 +0100

    creació de l'aplicació

--- ACTIVITAT_13/TIC_BCN_PMS/login/urls.py
+++ ACTIVITAT_13/TIC_BCN_PMS/login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('login', views.login, name="loginUsuari"),
+    path('main_page', views.main_page, name="paginaPrincipal"),
 ]
--- ACTIVITAT_13/TIC_BCN_PMS/login/views.py
+++ ACTIVITAT_13/TIC_BCN_PMS/login/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def main_page(request):
+    return render(request, 'main_page.html')
