1dae71b54b81c7febd730c8c3b5bd24bdaf07a89
getaudio2 <paugermaisar@hotmail.es>
2025-02-12 19:50:10 +0100

    formulari de login

--- ACTIVITAT_13/TIC_BCN_PMS/login/views.py
+++ ACTIVITAT_13/TIC_BCN_PMS/login/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    form = UserForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def main_page(request):
     return render(request, 'main_page.html')
