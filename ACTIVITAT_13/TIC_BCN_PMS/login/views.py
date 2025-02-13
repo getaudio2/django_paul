4574176bd238f407139b2556a78e8b1394528e17
getaudio2 <paugermaisar@hotmail.es>
2025-02-13 18:16:52 +0100

    vistes de login sense session

--- ACTIVITAT_13/TIC_BCN_PMS/login/views.py
+++ ACTIVITAT_13/TIC_BCN_PMS/login/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from wsgiref.util import request_uri
+
+from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import User
 
 # Create your views here.
 def login(request):
     form = UserForm()
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(email=email)
+            if user.password == password and not(user is None):
+                return redirect("paginaPrincipal")
+            else:
+                form.add_error(None, "Email o contrasenya incorrectes")
+        except User.DoesNotExist:
+            form.add_error(None, "Email o contrasenya incorrectes")
+
     context = {'form': form}
     return render(request, 'form.html', context)
 
